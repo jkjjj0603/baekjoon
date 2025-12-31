0356f9cdfebcffe20603ea27eb800beb8819fb6f
정우석 <jkjjj3795@gmail.com>
2025-12-31 14:49:21 +0900

    Solve Baekjoon 10162

---
+++ problems/b028.py
@@ -0,0 +1,17 @@
+import sys
+
+t = int(sys.stdin.readline().strip())
+
+if t % 10 != 0:
+    print(-1)
+else:
+    a = t // 300
+    t %= 300
+
+    b = t // 60
+    t %= 60
+
+    c = t // 10
+
+    print(a, b, c)
+    
