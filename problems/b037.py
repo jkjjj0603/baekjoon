566f94d47320fd99cd35ac14ab7408e10592381f
정우석 <jkjjj3795@gmail.com>
2026-01-02 18:37:54 +0900

    Fix solution for Baekjoon 1296

--- problems/b037.py
+++ problems/b037.py
@@ -1,6 +1,6 @@
 import sys
 
-yeaondu = sys.stdin.readline().strip()
+yeondu = sys.stdin.readline().strip()
 n = int(sys.stdin.readline().strip())
 
 best_name = ""
@@ -8,17 +8,17 @@ best_score = -1
 
 for _ in range(n):
     team = sys.stdin.readline().strip()
-    s = yeaondu + team
+    s = yeondu + team
 
     l_cnt = s.count("L")
     o_cnt = s.count("O")
     v_cnt = s.count("V")
     e_cnt = s.count("E")
 
-    score = (l_cnt+o_cnt) * (l_cnt+e_cnt) * (o_cnt+v_cnt) * (o_cnt+e_cnt) * (v_cnt+e_cnt)
+    score = (l_cnt+o_cnt) * (l_cnt+e_cnt) * (o_cnt+v_cnt) * (o_cnt+e_cnt) * (v_cnt+e_cnt) * (l_cnt+v_cnt)
     score = score % 100
 
-    if best_score > score:
+    if best_score < score:
         best_score = score
         best_name = team
     
