457291fe2642ced951f0c31d6b90f788054ac5bf
정우석 <jkjjj3795@gmail.com>
2026-01-02 18:29:23 +0900

    Solve Baekjoon 1296

---
+++ problems/b037.py
@@ -0,0 +1,29 @@
+import sys
+
+yeaondu = sys.stdin.readline().strip()
+n = int(sys.stdin.readline().strip())
+
+best_name = ""
+best_score = -1
+
+for _ in range(n):
+    team = sys.stdin.readline().strip()
+    s = yeaondu + team
+
+    l_cnt = s.count("L")
+    o_cnt = s.count("O")
+    v_cnt = s.count("V")
+    e_cnt = s.count("E")
+
+    score = (l_cnt+o_cnt) * (l_cnt+e_cnt) * (o_cnt+v_cnt) * (o_cnt+e_cnt) * (v_cnt+e_cnt)
+    score = score % 100
+
+    if best_score > score:
+        best_score = score
+        best_name = team
+    
+    elif score == best_score:
+        if team < best_name:
+            best_name = team
+
+print(best_name)
