c07ef02de032c0c7cef87fedca4983ea571f80de
정우석 <jkjjj3795@gmail.com>
2026-01-06 10:26:41 +0900

    Update p057

--- problems/p057.py
+++ problems/p057.py
@@ -3,15 +3,19 @@ import sys
 n = int(sys.stdin.readline().strip())
 five = n // 5
 ans = 0
+found = False
 
-while True:
+while five >= 0:
     rem = n - five * 5
-    if rem % 3 == 0:
+    if rem % 3== 0:
         three = rem // 3
         ans = five + three
         print(ans)
+        found = True
         break
-    else:
-        print(-1)
-        break
+    five -= 1
+
+if not found:
+    print(-1)
+
 
